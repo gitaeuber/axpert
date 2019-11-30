2c92f734f71097ef489bec051ff4fb18f515e92a
gitaeuber <lars.taeuber@web.de>
2019-11-30 23:56:26 +0100

    Update axpert.sh
    revert to previous commit
    because commit was wrong

--- axpert.sh
+++ axpert.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 #
-#
 # © Lars Täuber; AGPLv3 http://www.gnu.org/licenses/agpl-3.0.html
 #   lars.taeuber@web.de
 #
-# 2019-11-30	small optimizations
 # 2019-11-23	initial version
 #
 
 # serial settings: 2400 baud 8N1
 #
 # use SERVER and PORT if you connect via a ser2net server
-SERVER="ser2net"
+SERVER=ser2net
 PORT=3000
 #DEV="/dev/ttyUSB0"
 DEV="/dev/tcp/$SERVER/$PORT"
-LOGFILE="/srv/inverter/$(date +%Y-%m)-1.log"
-CMD="QPIGS"
-TRIES=2
+
 
 function axpert_crc16() {
     CRC_TA=($((0x0000)) $((0x1021)) $((0x2042)) $((0x3063)) $((0x4084)) $((0x50a5)) $((0x60c6)) $((0x70e7))
@@ -40,67 +36,61 @@ function axpert_crc16() {
     case "$CRC_L" in
 	$((0x28)) | $((0x0D)) | $((0x0A)) )
 	    ((CRC_L++));
-	    ;;
+	;;
     esac
 
     ((CRC_H=0xFF & (CRC>>8)));
     case "$CRC_H" in
 	$((0x28)) | $((0x0D)) | $((0x0A)) )
 	    ((CRC_H++));
-	    ;;
+	;;
     esac
 
     printf '\\x%02X\\x%02X\n' $((CRC_H)) $((CRC_L));
-}	# axpert_crc16()
-
-test -e "$LOGFILE" || \
-    echo "%Y%m%d%H%M%S U_GRID F_GRID U_OUT F_OUT VA_OUT P_OUT LOAD% U_BUS U_BATT I_BATT_IN C_BATT T_INV I_PV2BATT U_PV U_BATT_SCC I_BATT_OUT STATUS_BITS1 dU_FAN EEPROM P_PV STATUS_BITS2" >> "$LOGFILE";
-
-let I=10;
-while ! exec 5<>"$DEV"
-do
-    if [ $I -le 0 ]
-    then
-	echo "can't contact axpert inverter" >&2
-	exit 2
-    fi
-    let I--;
-    sleep 1;
-done
-
-function read_axpert() {
-    ## empty input buffer
-    read -n 30 -t 1 -u 5
-
-    echo -en "$CMD$(axpert_crc16 "$CMD")\r" >&5
-
-    if ! read -u 5 -t 2 -d '\r' LINE
-    then
-	# test for minimum length($LINE), start char "(" and CRC
-	if [ "${#LINE}" -ne 110 ]
-	then
-	    return 1
-	elif [ "${LINE:0:1}${LINE:(-3)}" != "($(echo -en "$(axpert_crc16 "${LINE:0:(${#LINE}-3)}")\r")" ]
-	then
-	    return 1
-	fi
-    fi
-    echo "$(date +%Y%m%d%H%M%S) ${LINE:1:(${#LINE}-4)}"
 }
 
 
-# try multiple times
-for ((I=0; I<TRIES; I++))
-do
-    if RESULT=$(read_axpert)
-    then
-	echo "$RESULT" >> "$LOGFILE";
-	break;
-    fi
-done
-
-if [ $I -eq $TRIES ]
+# parse device option
+if [ $# -ge 2 ]
 then
-    echo "couldn't read correct line from device" >&2
-    exit 1
+    case "$1" in
+	-d)
+	    DEV="$2"
+	    shift 2
+	    ;;
+    esac
 fi
+
+
+if [ $# -gt 0 ]
+then
+    exec 5<>"$DEV" || exit 1
+    sleep .25
+
+    ## empty input buffer
+    read -n 30 -t 1 -u 5
+
+    while [ $# -gt 0 ]
+    do
+	echo -en "$1\t"
+	echo -en "$1$(axpert_crc16 "$1")\r" >&5
+	read -u 5 -t 3 LINE
+	    # test for minimum length($LINE), start char "(" and CRC
+	    if [ "${#LINE}" -gt 3 -a "${LINE:0:1}${LINE:(-3)}" = "($(echo -en "$(axpert_crc16 "${LINE:0:(${#LINE}-3)}")\r")" ]
+	    then
+		echo "${LINE:1:$((${#LINE}-4))} "
+	    else
+		echo "answer incorrect"
+	    fi
+	shift
+    done
+    exec 5<&-
+else
+    echo "usage: $0 [-d DEV] CMD [[CMD] ...]"
+    echo "          DEV: device to connect to; e.g. /dev/ttyUSB0 or /dev/tcp/ser2net.de/PORT"
+    echo
+    echo " $0 sends a command CMD to axpert compatible inverter an prints the answer."
+    echo
+    exit 2
+fi
+
